377c7e67d5b69e4becaf0a83c13252a4d5c40fcf
zihuiye <zihuiye@gmail.com>
2016-04-22 19:13:39 +0800

    edge

--- Edge.java
+++ Edge.java
@@ -28,7 +28,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class WordCount {
+public class Edge {
 
   public static class SplitMapper 
        extends Mapper<Object, Text, Text, Text>{
