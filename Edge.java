a1b7c082e154213393a95bf2cf100a32a0299507
zihuiye <zihuiye@gmail.com>
2016-04-23 21:44:08 +0800

    Text

--- Edge.java
+++ Edge.java
@@ -135,13 +135,12 @@ public class Edge {
 	
 	public class decentComparator extends Text.Comparator {
 	
-		public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-			return -super.compare(b1, s1, l1, b2, s2, l2);
-		}
+		
 	
 		@Override  
-		public int compare(WritableComparable a, WritableComparable b) {  
-			return -super.compare(a,b);
+		public int compare(Text a, Text b) {  
+			
+			return b.compareTo(a);
 		}
 } 
 	
