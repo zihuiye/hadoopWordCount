b79fc67355dda32a3b9d2eb0d8dbacd9a1083d09
zihuiye <zihuiye@gmail.com>
2016-04-23 21:17:59 +0800

    constructor

--- Edge.java
+++ Edge.java
@@ -135,15 +135,18 @@ public class Edge {
 	
 	public class decentComparator extends WritableComparator {
 	
+		pubilc decentComparator(){
+			WritableComparator(Text);
+		}
 	
-	public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-        return -super.compare(b1, s1, l1, b2, s2, l2);
-    }
+		public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+			return -super.compare(b1, s1, l1, b2, s2, l2);
+		}
 	
-    @Override  
-    public int compare(WritableComparable a, WritableComparable b) {  
-        return -super.compare(a,b);
-    }
+		@Override  
+		public int compare(WritableComparable a, WritableComparable b) {  
+			return -super.compare(a,b);
+		}
 } 
 	
 	
